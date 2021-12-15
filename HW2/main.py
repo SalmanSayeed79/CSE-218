77558a3a9860b5ec961ebfa84982b8cb1881cc27
Salman Sayeed <66307068+SalmanSayeed79@users.noreply.github.com>
2021-12-15 12:51:19 +0600

    Created matrix to hold the constants

---
+++ HW2/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+def f(x,y){
+    sqr1=x**2
+    sqr2=y**2
+
+    return 
+}
+
+equationCount=int(input())
+matrix=np.array(equationCount,equationCount)
+for i in range(equationCount):
+    for j in matrix[i-1]:
+        matrix[i-1][j-1]=int(input())
+
+print(matrix)
